0c2bd37f843e13061aaf0caef2683171b99d8614
javier <javier.glez1493@gmail.com>
2025-08-18 20:53:50 -0600

    el usuario coloca numeros dentro de un arreglo y se verifica que no tenga algun numero repetido usando un ciclo for anidado

---
+++ src/main/java/Arreglos/repetidos.java
@@ -0,0 +1,28 @@
+
+package Arreglos;
+
+import java.util.Scanner;
+
+
+public class repetidos {
+  public static void main(String[] args) {
+        int arreglo[] = new int[5];
+        Scanner consola = new Scanner(System.in);
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Dame un numero");
+            arreglo[i] = consola.nextInt();
+
+        }
+
+        //mostrar
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = i + 1; j < arreglo.length; j++) {
+                if (arreglo[i] == arreglo[j]) {
+                    System.out.println("numero repetido:" + arreglo[i]);
+                }
+            }
+        }
+
+    }  
+}
